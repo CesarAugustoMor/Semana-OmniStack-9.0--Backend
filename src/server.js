1ca02eab4fe1f692bf232b9a0d962067b4977587
César Augusto Moro  Fürst <cesaramfurst@gmail.com>
2019-10-06 14:16:40 -0300

    adicionado caminho para visualização de thumbnail

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -15,5 +16,6 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 app.listen(8083);
