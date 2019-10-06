177f7fd33d35bd34343b51d5fd829259df65d268
César Augusto Moro  Fürst <cesaramfurst@gmail.com>
2019-10-06 12:41:58 -0300

    adionado cors

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 mongoose.connect(
-    "mongodb+srv://omnistack:omnistack@cluster0-rqaiw.mongodb.net/semana09?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
+  "mongodb+srv://omnistack:omnistack@cluster0-rqaiw.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
 );
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.listen(8083);
