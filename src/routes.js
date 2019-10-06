5a7b54b4b48d4a8d936b817175c047a71d632d2a
César Augusto Moro  Fürst <cesaramfurst@gmail.com>
2019-10-05 21:40:18 -0300

    adicionado funcionalidade de cadastrar spot

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+
 const SessionControler = require("./controllers/SessionController");
+const SpotControler = require("./controllers/SpotController");
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.post("/sessions", SessionControler.store);
+routes.post("/spots", upload.single("thumbnail"), SpotControler.store);
 
 module.exports = routes;
